a87e6559746dbffaceba2ae6393be421fe2b131f
Jazper26 <jazperhaapin89Gmail.com>
2024-01-26 21:23:04 +0800

    Printing the result

--- Exercise 7.py
+++ Exercise 7.py
@@ -4,3 +4,5 @@ str_x = "Emma is good developer. Emma is a writer"
 # Count the occurrences of the substring "Emma"
 count = str_x.count("Emma")
 
+# Print the result
+print(f"Emma appeared {count} times")
